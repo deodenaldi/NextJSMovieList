207f530a8cbdcfd2b3cefd66986e54866009c2e0
deodenaldi <58246866+deodenaldi@users.noreply.github.com>
2022-06-12 16:12:08 +0700

    Penambahan intro

---
+++ components/Intro.js
@@ -0,0 +1,21 @@
+import { Container, Row, Col } from "react-bootstrap";
+
+const Intro = () => {
+    return ( <
+        div className = 'Intro' >
+        <
+        Container className = 'text-dark d-flex justify-content-center align-items-center border' >
+        <
+        Row >
+        <
+        Col >
+        <
+        div className = 'title' > Review Film < /div> <
+        div className = 'title' > Kesukaanmu < /div> < /
+        Col > <
+        /Row> < /
+        Container > < /div> 
+    )
+}
+
+export default Intro
